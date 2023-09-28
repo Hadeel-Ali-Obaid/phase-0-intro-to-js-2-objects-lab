c9656c049a14f6168988a7a3e1ba7a23b6546a63
Hadeel Obaid <hadeelobaid71@gmail.com>
2023-09-28 12:28:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution in this file!
+let employee ={
+    name: "hadeel",
+    streetAddress: "abuNsair"
+}
+
+const updateEmployeeWithKeyAndValue = function(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+      };
+    }
+
+
+function nondestructivelyUpdateObject(obj, key, value) {
+    return {
+      ...employee,
+      [key]: value,
+    };
+  }
+  
+  const new1 = nondestructivelyUpdateObject(
+    employee,
+    "name",
+    "sam",
+    "street",
+    "11 Broadway"
+  );
+   
+
+
+   const destructivelyUpdateEmployeeWithKeyAndValue = function() {
+    employee.streetAddress= "12 Broadway";
+    return employee
+  }
+
+
+
+  let newEmployee
+  const deleteFromEmployeeByKey= function(employee, key) {
+    newEmployee = {...employee};
+    delete newEmployee.name; 
+    return newEmployee;
+  }
+
+
+  
+  const destructivelyDeleteFromEmployeeByKey= function(employee, key) {
+   delete employee.name;
+   return employee;
+  }
